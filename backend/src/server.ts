a1cdae21a0547e8003a88ced46f4e4c323dfe26a
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-08-18 16:19:57 +0200

    Server ready for use

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser, { json } from 'body-parser';
+import mongoose from 'mongoose';
+import user from './model/user';
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/MEAN_STACK');
+
+const conn = mongoose.connection;
+
+conn.once('open', ()=>{
+    console.log('mongo open');
+})
+
+const router = express.Router();
+
+app.use('/', router);
 app.listen(4000, () => console.log(`Express server running on port 4000`));
